1fee65a44bc7c57c5b8290180f37b7091367e0e1
Vytautas <vytautas.zabielskas@gmail.com>
2023-02-21 17:49:52 +0200

    Added AuthorsController

--- BookStoreApp.API/Data/Author.cs
+++ BookStoreApp.API/Data/Author.cs
@@ -5,6 +5,11 @@ namespace BookStoreApp.API.Data;
 
 public partial class Author
 {
+    public Author() 
+    { 
+        Books = new HashSet<Book>();
+    }
+
     public int Id { get; set; }
 
     public string? FirstName { get; set; }
--- BookStoreApp.API/Data/BookStoreDbContext.cs
+++ BookStoreApp.API/Data/BookStoreDbContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
 
 namespace BookStoreApp.API.Data;
 
